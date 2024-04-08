3bc289dad98f9321e96ee81700cb6f08f7d009a5
mooooonoe <msb3536@naver.com>
2024-04-08 20:16:48 +0900

    [3190 성공]뱀

--- 3190.py
+++ 3190.py
@@ -1,35 +1,55 @@
-
 n = int(input())
-board = [([0]*n) for i in range(n)]
-numApple = int(input())
-for i in range(numApple):
-    x,y = map(int, input().split())
-    board[x][y] = 1
+board = [([0]*n) for _ in range(n)]
 
-#  → ↓ ← ↑
-dx = [1, 0, -1, 0 ]
-dy = [0, -1, 0, 1 ]
-dir = 1
-nx, ny = 0,0
+k = int(input())
+for _ in range(k):
+    x,y = map(int, input().split())
+    board[x-1][y-1] = 2
 
 L = int(input())
-for i in range(L):
-    when, dir = input().split()
-    times[int(when)] = dir          #### times
+times = [0]*10000
+for _ in range(L):
+    when,dirct = input().split()
+    times[int(when)] = dirct
 
+snake=[]
+snake.append([0,0])
+board[0][0] = 1
 
-# print(board)
+# 위 오른쪽 아래 왼쪽
+# 0     1  2    3
+dx = [-1, 0, 1, 0]
+dy = [0, 1, 0, -1]
+dirct = 1
 
-
+nx,ny = 0,0
 
 time = 0
-def go_snake():
-    global time
+
+while(True):
     time += 1
+    nx = nx + dx[dirct]
+    ny = ny + dy[dirct]
 
-Dummy = 1
+    if(nx<0 or ny<0 or nx>=n or ny>=n or board[nx][ny]==1):
+        break
+
+    if(board[nx][ny] == 2):
+        snake.append([nx,ny])
+        board[nx][ny] = 1
+
+    elif(board[nx][ny] == 0):
+        snake.append([nx,ny])
+        board[nx][ny] = 1
+
+        delX,delY = snake.pop(0) # 처음 시작한 꼬리 비우기
+        board[delX][delY] = 0
+
+    if(times[time] =='D'):
+        dirct = (dirct + 1) % 4
+    elif(times[time] =='L'):
+        dirct = (dirct + 3) % 4
+
+print(time)
 
 
-numTurn = int(input())
-
-for i in range(numTurn):
--- test.py
+++ test.py
@@ -1,1 +1,38 @@
-print('hi')
+# when, dir = input().split()
+# print(when, dir)
+# print(type(dir))
+# print(type(int(when)))
+#
+
+####
+
+
+# 위 오른쪽 아래 왼쪽
+# 0     1  2    3
+dx = [-1, 0, 1, 0]
+dy = [0, 1, 0, -1]
+dirct = 1
+
+nx, ny = 0, 0
+print('현위치', nx, ny)
+
+nx = nx + dx[dirct]
+ny = ny + dy[dirct]
+
+print(dx[dirct], dy[dirct])
+print('오른쪽 이동후', nx, ny)
+
+dirct = (dirct+1)%4
+nx = nx + dx[dirct]
+ny = ny + dy[dirct]
+
+print(dx[dirct], dy[dirct])
+print('아래 이동후', nx, ny)
+
+dirct = (dirct+3)%4
+
+nx = nx + dx[dirct]
+ny = ny + dy[dirct]
+
+print(dx[dirct], dy[dirct])
+print('왼쪽 이동후', nx, ny)
