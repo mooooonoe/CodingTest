8286358735a3aff4790db2156bcf46ccaf6a2b99
mooooonoe <msb3536@naver.com>
2024-04-09 11:47:30 +0900

    기호 갯수 , 기호 순서

--- 14888.py
+++ 14888.py
@@ -12,32 +12,37 @@ for i in range(4):
     if(tools[i]==0):
         continue
 
-print(org)
-#
-# global minResult
-# global maxResult
-#
-# minResult = -1e9
-# maxResult = 1e9
-#
-#
-# def permutation(arr, r):
-#     result = []
-#     arr.sort()
-#     def gen_permutation(elements, r, path=[]):
-#         if r == 0:
-#             result.append(path)
-#             return
-#
-#         for i in range(len(elements)):
-#             if i > 0 and elements[i] == elements[i-1]:
-#                 continue
-#             gen_permutation(elements[:i]+elements[i+1:], r - 1, path + [elements[i]])
-#
-#     gen_permutation(arr,r)
-#     return result
-#
-# # print(permutation([1, 2, 4, 3, 2], 3))
+global minResult
+global maxResult
+
+minResult = -1e9
+maxResult = 1e9
+
+def permutation(arr, r):
+    result = []
+    arr.sort()
+    def gen_permutation(elements, r, path=[]):
+        if r == 0:
+            result.append(path)
+            return
+
+        for i in range(len(elements)):
+            if i > 0 and elements[i] == elements[i-1]:
+                continue
+            gen_permutation(elements[:i]+elements[i+1:], r - 1, path + [elements[i]])
+
+    gen_permutation(arr,r)
+    return result
+
+Ori = list(permutation(org, N-1))
+print(Ori[0])
+print(Ori[1])
+
+# for i in range(N-1):
+#     permutation(num(i),num(i+1),org(i))
+
+
+
 #
 # class ToolMath:
 #     def plustool(a,b) -> int:
@@ -48,11 +53,8 @@ print(org)
 #         return a*b
 #     def devitool(a,b) -> int:
 #         return a/b
-#
-#
-#
-# for _ in range(num):
-#
-#     tool(num(N),num(N+1), ())
+
+
+
 
 
