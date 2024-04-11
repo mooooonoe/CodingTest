448757e254af1b5363973e6a5dd0a4ce629f4f8c
mooooonoe <msb3536@naver.com>
2024-04-11 16:40:48 +0900

    [14499 실패] 주사위 굴리기 / 주사위 dir 매핑 다시

--- 14499.py
+++ 14499.py
@@ -1,31 +1,29 @@
-row,col,x,y,k = map(int, input().split())
-board = []
+row, col, x, y, k = map(int, input().split())
 
-dx = [0,0,-1,1]
-dy = [1,-1,0,0]
+# 동 서 남 북
+dx = [0, 0, -1, 1]
+dy = [1, -1, 0, 0]
 rect = [0,0,0,0,0,0]
-
-# 주사위 mapping
 
 def turn(dir):
     a, b, c, d, e, f = rect[0], rect[1], rect[2], rect[3], rect[4], rect[5]
     if dir == 1:
-        rect[0], rect[1], rect[2], rect[3], rect[4], rect[5] = d, b, a, f, e, c
+        rect[0], rect[1], rect[2], rect[3], rect[4], rect[5] = d, a, c, d, e, f
     elif dir == 2:  # 서
-        rect[0], rect[1], rect[2], rect[3], rect[4], rect[5] = c, b, f, a, e, d
+        rect[0], rect[1], rect[2], rect[3], rect[4], rect[5] = b, c, d, a, e, f
     elif dir == 3:  # 북
-        rect[0], rect[1], rect[2], rect[3], rect[4], rect[5] = e, a, c, d, f, b
+        rect[0], rect[1], rect[2], rect[3], rect[4], rect[5] = e, b, f, d, c, a
     else:
-        rect[0], rect[1], rect[2], rect[3], rect[4], rect[5] = b, f, c, d, a, e
+        rect[0], rect[1], rect[2], rect[3], rect[4], rect[5] = f, b, e, d, a, c
 
-# 지도 mapping
+board = []
+
 for i in range(row):
     board.append(list(map(int, input().split())))
 
-# move
 comm = list(map(int, input().split()))
 
-nx,ny = x,y
+nx, ny = x, y
 
 for i in comm:
     nx += dx[i-1]
@@ -36,11 +34,13 @@ for i in comm:
         ny -= dy[i-1]
         continue
     turn(i)
+
     if board[nx][ny] == 0:
-        board[nx][ny] = rect[-1]
+        board[nx][ny] = rect[2]
     else:
-        rect[-1] = board[nx][ny]
+        rect[2] = board[nx][ny]
         board[nx][ny] = 0
 
     print(rect[0])
+
 
