b10d98ed8b5270463b56d8980fd9ea19301a518d
mooooonoe <msb3536@naver.com>
2024-04-11 15:18:34 +0900

    [13458 실패] 시험감독 / while 문 써서 시간초과?

--- 13458.py
+++ 13458.py
@@ -2,17 +2,16 @@ N = int(input())
 tester = list(map(int, input().split()))
 a,b = map(int, input().split())
 
-cnt = [0]*N
-total = 0
+
+cnt = 0
 
 for i in range(N):
     tester[i] -= a
-    cnt[i] += 1
+    cnt += 1
     while tester[i] > 0:
         tester[i] -= b
-        cnt[i] += 1
+        cnt += 1
         if tester[i] <= 0:
-            total += cnt[i]
             break
 
-print(total)
+print(cnt)
