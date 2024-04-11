74b749806ed9775579dd460e3fe7cfdb97abd210
mooooonoe <msb3536@naver.com>
2024-04-11 21:04:31 +0900

    [14499 성공] 주사위 돌리기 / dir idx 1부터 시작

--- 14499.py
+++ 14499.py
@@ -5,39 +5,40 @@ for i in range(n):
     board.append(list(map(int, input().split())))
 
 # 동 서 남 북
-dx = [1, -1, 0, 0]
-dy = [0, 0, 1, -1]
+dx = [0, 0, -1, 1]
+dy = [1, -1, 0, 0]
 
 nx = x; ny = y
 
 rect = [0]*6
 def turn(dir):
     a, b, c, d, e, f = rect[0], rect[1], rect[2], rect[3], rect[4], rect[5]
-    if dir == 0:
+    if dir == 1:
         rect[0], rect[1], rect[2], rect[3], rect[4], rect[5] = d, b, a, f, e, c
-    elif dir == 1:
-        rect[0], rect[1], rect[2], rect[3], rect[4], rect[5] = c, b, f, a, e, d
     elif dir == 2:
+        rect[0], rect[1], rect[2], rect[3], rect[4], rect[5] = c, b, f, a, e, d
+    elif dir == 3:
         rect[0], rect[1], rect[2], rect[3], rect[4], rect[5] = e, a, c, d, f, b
     else :
         rect[0], rect[1], rect[2], rect[3], rect[4], rect[5] = b, f, c, d, a, e
-
-    return rect
+    #
+    # return rect
 
 comm = list(map(int, input().split()))
 
 for i in comm:
-    nx += dx[i-1]
-    ny += dx[i-1]
+    nx += dx[i-1]   # idx 시작이 0이라서
+    ny += dy[i-1]
 
-    turn(dir)
+    if nx < 0 or nx >= n or ny < 0 or ny >= m:
+        nx -= dx[i-1]
+        ny -= dy[i-1]
+        continue
+    turn(i)
+    if board[nx][ny] == 0:
+        board[nx][ny] = rect[-1]
 
-
-
-    if board[nx][ny]  == 0:
-        board[nx][ny]  = rect[-1]
-
-    else :
+    else:
         rect[-1] = board[nx][ny]
         board[nx][ny] = 0
 
