5efed6729599fd3f7c679b221375ec1f32e3626e
mooooonoe <msb3536@naver.com>
2024-04-11 20:07:19 +0900

    [14501 성공] 퇴사 / bottom up

--- 14501.py
+++ 14501.py
@@ -1,23 +1,21 @@
 day = int(input())
-arrT = []
-arrP = []
+T = [0]*day; P = [0]*day
 
 for i in range(day):
-    T,P = map(int, input().split())
-    arrT.append(T)
-    arrP.append(P)
-
-# print(arrT, arrP)
-# dp = [0 for i in range(day+1)]
+    a, b = map(int, input().split())
+    T[i] = a
+    P[i] = b
 
 dp = [0]*(day+1)
-max_value = 0
+max_val = 0
 
 for i in range(day-1, -1, -1):
-    if arrT[i]+i <= day:
-        dp[i] = max(arrP[i] + dp[i+arrT[i]], max_value)
-        max_value = dp[i]
+    if T[i]+i <= day:
+        dp[i] = max(P[i]+dp[i+T[i]], max_val)
+        max_val = dp[i]
     else:
-        dp[i] = max_value
+        dp[i] = max_val
 
-print(max_value)
+print(max_val)
+
+
