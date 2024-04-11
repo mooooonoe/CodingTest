c4d35aa81709f237708b836e1e73b3aa49da1f9d
mooooonoe <msb3536@naver.com>
2024-04-11 15:09:31 +0900

    [13458 실패] 시험감독 / while 문 쓰지마

--- test.py
+++ test.py
@@ -1,21 +1,18 @@
-row, col = map(int, input().split())
+# 13458 시험감독
 
-# 북 동 남 서
-dx = [-1, 0, 1, 0]
-dy = [0, 1, 0, -1]
+N = int(input())
+tester = list(map(int, input().split()))
 
-nx, ny, direction = map(int, input().split())
+a, b = map(int, input().split())
 
-room = [list(map(int, input().split())) for _ in range(row)]
-
-rect = 0
-
-while True:
-    # 현재 칸 청소
-    if room[nx][ny] == 0:
-        room[nx][ny] = 2  # 청소 표시
-        rect += 1
-
-    # 4방향 모두 청소되었거나 벽인 경우 후진 또는 작동 중지
-    if room[nx - 1][ny] != 0 and room[nx][ny + 1] != 0 and room[nx + 1][ny] != 0 and room[nx][ny - 1] != 0:
-        if room[nx - dx[direction]][JIEJROP2JFI
+cnt = 0
+for i in range(N):
+    cnt = 1
+    tester[i] -= a
+    while tester[i]>0:
+        tester[i] -= b
+        cnt += 1
+        if tester[i]<=0:
+            print(cnt)
+        else:
+            continue
