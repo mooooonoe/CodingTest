5740b76155b960b5a8c7871f9e0fd50c791ca7a9
mooooonoe <msb3536@naver.com>
2024-04-11 15:15:55 +0900

    [13458 실패] 시험감독 / cnt, total 구분해서 시간초과

--- 13458.py
+++ 13458.py
@@ -2,14 +2,17 @@ N = int(input())
 tester = list(map(int, input().split()))
 a,b = map(int, input().split())
 
+cnt = [0]*N
 total = 0
+
 for i in range(N):
     tester[i] -= a
-    total += 1
-    if tester[i] > 0:
-        if tester[i]%b == 0:
-            total += (tester[i])//b
-        else:
-            total += tester[i]//b+1
+    cnt[i] += 1
+    while tester[i] > 0:
+        tester[i] -= b
+        cnt[i] += 1
+        if tester[i] <= 0:
+            total += cnt[i]
+            break
 
 print(total)
