0b930d876299b8cc8ebb66753b7130851067294a
mooooonoe <msb3536@naver.com>
2024-04-13 21:23:27 +0900

    [14499 성공] 주사위 돌리기 / 헤헤 일단 돌리고 조건에 안맞으면 -=

--- 14499.py
+++ 14499.py
@@ -1,14 +1,11 @@
 n, m, x, y, k = map(int, input().split())
+board = [list(map(int, input().split())) for _ in range(n)]
 
-board = []
-for i in range(n):
-    board.append(list(map(int, input().split())))
+op_num = list(map(int, input().split()))
 
-# 동 서 남 북
+    # 동 서 남 북
 dx = [0, 0, -1, 1]
 dy = [1, -1, 0, 0]
-
-nx = x; ny = y
 
 rect = [0]*6
 def turn(dir):
@@ -19,29 +16,33 @@ def turn(dir):
         rect[0], rect[1], rect[2], rect[3], rect[4], rect[5] = c, b, f, a, e, d
     elif dir == 3:
         rect[0], rect[1], rect[2], rect[3], rect[4], rect[5] = e, a, c, d, f, b
-    else :
+    else:
         rect[0], rect[1], rect[2], rect[3], rect[4], rect[5] = b, f, c, d, a, e
-    #
-    # return rect
 
-comm = list(map(int, input().split()))
+    return rect
 
-for i in comm:
-    nx += dx[i-1]   # idx 시작이 0이라서
+nx = x
+ny = y
+
+for i in op_num:
+    nx += dx[i-1]
     ny += dy[i-1]
 
-    if nx < 0 or nx >= n or ny < 0 or ny >= m:
+    if nx < 0 or nx >= n or ny <0 or ny>=m:
         nx -= dx[i-1]
         ny -= dy[i-1]
         continue
+
     turn(i)
     if board[nx][ny] == 0:
         board[nx][ny] = rect[-1]
-
+        print(rect[0])
     else:
         rect[-1] = board[nx][ny]
         board[nx][ny] = 0
+        print(rect[0])
 
-    print(rect[0])
+
+
 
 
